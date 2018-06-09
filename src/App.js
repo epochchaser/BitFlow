37624bc0cafbd66751d32c84af60bad8fc9d7ef3
chaser <epochchaser@gmail.com>
2018-06-10 01:37:53 +0900

    basic graph from  bitcoin dummy data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import LineChart from './components/LineChart';
 
 class App extends Component {
+  createDummyGraphData(){
+    const data = []
+    for (let x = 0; x <= 30; x++) {
+      const random = Math.random();
+      const temp = data.length > 0 ? data[data.length-1].y : 50;
+      const y = random >= .45 ? temp + Math.floor(random * 20) : temp - Math.floor(random * 20);
+      data.push({x,y})
+    }
+    return data;
+  }
+
   render() {
     return (
       <div className="App">
+        <div>Expect bitcoin price</div>
+        <LineChart data={this.createDummyGraphData()}/>
       </div>
     );
   }
